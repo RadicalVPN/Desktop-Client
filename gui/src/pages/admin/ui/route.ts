5c6407443cb13b9be9688b864b8110566fe8e22e
Primexz <johann@pepebot.info>
2023-11-03 15:28:32 +0100

    chore: remove unused pages

--- gui/src/pages/admin/ui/route.ts
+++ gui/src/pages/admin/ui/route.ts
@@ -4,11 +4,5 @@ export default {
   name: 'ui',
   path: 'ui',
   component: RouteViewComponent,
-  children: [
-    {
-      name: 'typography',
-      path: 'typography',
-      component: () => import('../../../pages/admin/ui/typography/Typography.vue'),
-    },
-  ],
+  children: [],
 }
