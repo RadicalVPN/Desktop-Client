7f4366907da44f88e36d0329505c58e141b82a57
Primexz <johann@pepebot.info>
2023-11-09 00:43:38 +0100

    feat(installer): add installed check

--- build/darwin/daemon-installer/daemon-installer.c
+++ build/darwin/daemon-installer/daemon-installer.c
@@ -40,8 +40,7 @@ int install_daemon() {
                                kAuthorizationFlagPreAuthorize |
                                kAuthorizationFlagExtendRights;
 
-    const char *prompt = "Penis.\n\n";
-
+    const char *prompt = "RadicalVPN requires a background service to establish VPN connections.\nIt is not possible to use RadicalVPN without the background service.\n\n";
 
     AuthorizationItem envItems = {kAuthorizationEnvironmentPrompt, strlen(prompt), (void *)prompt, 0};
     AuthorizationEnvironment env = { 1, &envItems };
@@ -52,14 +51,14 @@ int install_daemon() {
     if(err == errAuthorizationSuccess)
     {
         //add the daemon starter the LaunchD
-        bool isSuccess = SMJobBless(kSMDomainSystemLaunchd,
+        bool success = SMJobBless(kSMDomainSystemLaunchd,
                       CFStringCreateWithCString(kCFAllocatorDefault, RADICAL_PACKAGE_NAME, kCFStringEncodingMacRoman),
                       (AuthorizationRef) authRef,
                       &error);
 
         AuthorizationFree(authRef, kAuthorizationFlagDefaults);
 
-        if (isSuccess)
+        if (success)
         {
             printf("Daemon installed.");
             return 0;
@@ -76,10 +75,93 @@ int install_daemon() {
         }
     }
 
-
-    printf("err: %d\n", err);
+    return 1;
 }
 
+int get_installed_bundle_path(char* ret_buf, int buf_size) {
+    CFStringRef helper_str = CFStringCreateWithCString(kCFAllocatorDefault, RADICAL_PACKAGE_NAME, kCFStringEncodingMacRoman);
+    CFDictionaryRef retDict = SMJobCopyDictionary(kSMDomainSystemLaunchd, helper_str);
+    if (helper_str != NULL)
+    {
+        CFRelease(helper_str);
+    }
+
+    if (retDict == NULL)
+    {
+        return 1;
+    }
+
+    int ret = 0;
+    CFStringRef key = CFStringCreateWithCString(kCFAllocatorDefault, "ProgramArguments", kCFStringEncodingMacRoman);
+    if (CFDictionaryContainsKey(retDict, key))
+    {
+        CFArrayRef program_arguments = CFDictionaryGetValue(retDict, key);
+        if (program_arguments != NULL)
+        {
+            CFStringRef helperBundlePath = CFArrayGetValueAtIndex(program_arguments, 0);
+            if (!CFStringGetCString(helperBundlePath, ret_buf, buf_size, kCFStringEncodingMacRoman))
+            {
+                ret = 2;
+            }
+        }
+        else
+        {
+            ret = 3;
+        }
+    }
+    else
+    {
+        ret = 4;
+    }
+
+    if (key!=NULL)
+    {
+        CFRelease(key);
+    }
+
+    if (retDict!=NULL)
+    {
+        CFRelease(retDict);
+    }
+
+    return ret;
+}
+
+bool daemon_install_required() {
+    char installed_bundle[256] = {0};
+
+    if (get_installed_bundle_path(installed_bundle, 256) == 0)
+    {
+        printf("%s\n", installed_bundle);
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+
 int main(int argc, char **argv) {
-    install_daemon();
+    if (argc >= 2)
+    {
+        if (strcmp(argv[1], "--install") == 0)
+        {
+            printf("Installing daemon..");
+            return install_daemon();
+        }
+
+        if (strcmp(argv[1], "--install-required") == 0)
+        {
+            bool install_required = daemon_install_required();
+            install_required ? printf("Daemon install required") : printf("Daemon install not required");
+            return install_required ? 0 : 1;
+        }
+    }
+
+    // no matching option
+    printf("Invalid usage.\n");
+    printf("Arguments:\n");
+    printf("    --install\n");
+
+    return 1;
 }
