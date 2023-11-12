dffcd2dd1eb79bd7a067a117b993c9c1331fcc83
Primexz <johann@pepebot.info>
2023-11-13 00:56:30 +0100

    feat(privacy_firewall): some improvements

---
+++ gui/src/pages/admin/privacyfirewall/Information.vue
@@ -0,0 +1,19 @@
+<template>
+  <va-badge overlap>
+    <template #text> {{ t('new') }} </template>
+    <va-card>
+      <va-card-title style="font-size: 0.875rem">{{ t('privacyFirewall.info.title') }}</va-card-title>
+      <va-card-content class="space-y-2">
+        <p>{{ t('privacyFirewall.info.description.1') }}</p>
+        <p>{{ t('privacyFirewall.info.description.2') }}</p>
+        <p>{{ t('privacyFirewall.info.description.3') }}</p>
+      </va-card-content>
+    </va-card>
+  </va-badge>
+</template>
+
+<script lang="ts" setup>
+  import { useI18n } from 'vue-i18n'
+
+  const { t } = useI18n()
+</script>
--- gui/src/pages/admin/privacyfirewall/PrivacyFirewall.vue
+++ gui/src/pages/admin/privacyfirewall/PrivacyFirewall.vue
@@ -1,57 +1,15 @@
 <template>
-  <va-card class="mb-8">
-    <va-card-title>Information</va-card-title>
+  <h1 style="color: var(--va-text-primary)" class="text-xl mb-6">{{ t('privacyFirewall.title') }}</h1>
 
-    <va-card-content>
-      <va-collapse v-model="test" header="Collapse header"> Collapse content </va-collapse></va-card-content
-    >
-  </va-card>
-
-  <va-card class="mb-8">
-    <va-card-title>Configuration</va-card-title>
-
-    <va-card-content>
-      <va-select
-        v-model="store.privacyFirewallLevel"
-        class="mb-6"
-        label="asdasd"
-        :options="levels"
-        text-by="textBy"
-        value-by="text"
-      />
-    </va-card-content>
-
-    {{ store.privacyFirewallLevel }}
-  </va-card>
+  <privacy-firewall-information class="mb-8" />
+  <privacy-firewall-configuration />
+  <!--  -->
 </template>
 
 <script lang="ts" setup>
   import { useI18n } from 'vue-i18n'
-  import { useGlobalStore } from '../../../stores/global-store'
-  import { SelectableOption } from 'vuestic-ui/dist/types/composables'
-  import { ref } from 'vue'
+  import PrivacyFirewallInformation from './Information.vue'
+  import PrivacyFirewallConfiguration from './Configuration.vue'
 
   const { t } = useI18n()
-  const store = useGlobalStore()
-
-  const test = ref()
-
-  const levels = [
-    {
-      text: 'basic',
-      textBy: t('privacyFirewall.basic'),
-    },
-    {
-      text: 'recommended',
-      textBy: t('privacyFirewall.recommended'),
-    },
-    {
-      text: 'comprehensive',
-      textBy: t('privacyFirewall.comprehensive'),
-    },
-    {
-      text: 'agressive',
-      textBy: t('privacyFirewall.agressive'),
-    },
-  ] as SelectableOption[]
 </script>
