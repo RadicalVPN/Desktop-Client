0ec695b13e4c0324ef7ff246d4d7d582e8dd1477
Primexz <johann@pepebot.info>
2023-11-13 21:14:34 +0100

    refactor: remove unused code

--- gui/src/main.ts
+++ gui/src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import i18n from './i18n'
 import { createVuestic } from 'vuestic-ui'
-import { createGtm } from '@gtm-support/vue-gtm'
 
 import stores from './stores'
 import router from './router'
@@ -14,16 +13,6 @@ app.use(stores)
 app.use(router)
 app.use(i18n)
 app.use(createVuestic({ config: vuesticGlobalConfig }))
-
-if (import.meta.env.VITE_APP_GTM_ENABLED) {
-  app.use(
-    createGtm({
-      id: import.meta.env.VITE_APP_GTM_KEY,
-      debug: false,
-      vueRouter: router,
-    }),
-  )
-}
 
 app.mount('#app').$nextTick(() => {
   postMessage({ payload: 'removeLoading' }, '*')
