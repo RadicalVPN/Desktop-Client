19825004ec852022fcaf709ee58a5e4232166452
Primexz <johann@pepebot.info>
2023-12-17 21:30:38 +0100

    fix: update signup btn

--- gui/src/pages/auth/login/Login.vue
+++ gui/src/pages/auth/login/Login.vue
@@ -46,6 +46,8 @@
   import { useI18n } from 'vue-i18n'
   import axios from 'axios'
   import { DaemonHelper } from '../../../helper/daemon'
+  import { shell } from 'electron'
+  import { onMounted } from 'vue'
 
   const { t } = useI18n()
 
--- gui/src/pages/auth/signup/Signup.vue
+++ gui/src/pages/auth/signup/Signup.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="flex flex-col items-center">
-    <va-button>https://radicalvpn.com</va-button>
+    <va-button @click="openPage">Signup now</va-button>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { shell } from 'electron'
+
+  function openPage() {
+    shell.openExternal('https://radicalvpn.com/portal/auth/signup')
+  }
+</script>
