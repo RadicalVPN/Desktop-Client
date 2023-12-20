6d5571dfcd5d5320c0cece89c9ec22ad577382d8
Primexz <johann@pepebot.info>
2023-12-20 23:15:54 +0100

    fix(gui): make privacy firewall reload smooth

--- gui/src/pages/admin/privacyfirewall/Analytics.vue
+++ gui/src/pages/admin/privacyfirewall/Analytics.vue
@@ -9,8 +9,7 @@
       <va-card-content class="flex items-center">
         <div class="flex-1">
           <p class="text-white pb-2">{{ statistic.title }}</p>
-          <VaSkeleton v-if="isLoading" variant="text" class="va-h2 m-1 w-20" />
-          <h2 v-else class="va-h2 m-0 text-white">{{ statistic.value }}</h2>
+          <h2 class="va-h2 m-0 text-white">{{ statistic.value }}</h2>
         </div>
         <div class="absolute top-10 right-3">
           <va-icon :name="statistic.icon" class="opcacity-25" color="secondary" :size="60" />
@@ -23,24 +22,23 @@
   import { onMounted, ref } from 'vue'
   import { DaemonHelper } from '../../../helper/daemon'
   import { onBeforeUnmount } from 'vue'
-  const isLoading = ref(false)
   let refreshTimer: any
   const statistics = ref([
     {
       title: 'Total queries',
-      value: '0',
+      value: 'N/A',
       icon: 'fa-globe',
       color: '#33a65a',
     },
     {
       title: 'Queries Blocked',
-      value: '0',
+      value: 'N/A',
       icon: 'fa-hand',
       color: '#41c0ef',
     },
     {
       title: 'Percent Blocked',
-      value: '0',
+      value: 'N/A',
       icon: 'fa-chart-pie',
       color: '#f39c12',
     },
@@ -48,7 +46,6 @@
 
   async function updateStatistics() {
     console.log('updating privacy firewall stats')
-    isLoading.value = true
 
     const stats = await new DaemonHelper().getPrivacyFirewallStats()
     if (stats.status) {
@@ -60,15 +57,13 @@
       statistics.value[1].value = blocked.toString()
       statistics.value[2].value = `${((blocked / total) * 100).toFixed(2).toString()}%`
     }
-
-    isLoading.value = false
   }
 
   onMounted(async () => {
     await updateStatistics()
     refreshTimer = setInterval(async () => {
       await updateStatistics()
-    }, 30_000)
+    }, 30_000) //backend caches 60 secs; let's refresh every 30 secs
   })
 
   onBeforeUnmount(() => {
