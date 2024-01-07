aff4cdf470fdb0cbb2bf0f08981c627c9f4c82e6
Primexz <johann@pepebot.info>
2024-01-07 23:20:56 +0100

    feat(daemon): compare versions on darwin

--- build/darwin/daemon-installer/daemon-installer.c
+++ build/darwin/daemon-installer/daemon-installer.c
@@ -1,168 +1,164 @@
-#include <syslog.h>
-#include <string.h>
-#include <stdio.h>
-#include <ServiceManagement/ServiceManagement.h>
 #include <Security/Security.h>
+#include <ServiceManagement/ServiceManagement.h>
+#include <stdio.h>
+#include <string.h>
+#include <syslog.h>
 
 #define RADICAL_PACKAGE_NAME "com.radicalvpn.booter.helper"
+#define INSTALLED_HELPER_BINARY \
+	"/Library/PrivilegedHelperTools/com.radicalvpn.booter.helper"
+#define BUNDLE_HELPER_BINARY \
+	"/Applications/RadicalVPN.app/Contents/MacOS/RadicalVPN-Installer.app/Contents/Library/LaunchServices/com.radicalvpn.booter.helper"
+
 
 void log_error_ref(CFErrorRef err) {
 	if (err == NULL)
 		return;
 
-    CFStringRef error_txt = CFErrorCopyDescription(err);
-    if (error_txt == NULL)
-        return;
+	CFStringRef error_txt = CFErrorCopyDescription(err);
+	if (error_txt == NULL)
+		return;
 
-	const char  *ptr      = CFStringGetCStringPtr(error_txt, kCFStringEncodingUTF8);
-    if (ptr == NULL)
-    {
-        const CFIndex bufSize = 1024;
-        char buf[bufSize];
+	const char* ptr = CFStringGetCStringPtr(error_txt, kCFStringEncodingUTF8);
+	if (ptr == NULL) {
+		const CFIndex bufSize = 1024;
+		char buf[bufSize];
 
-        if (CFStringGetCString(error_txt, buf, bufSize, kCFStringEncodingUTF8))
-        {
-            printf("%s", buf);
-        }
-        return;
-    }
+		if (CFStringGetCString(error_txt, buf, bufSize, kCFStringEncodingUTF8)) {
+			printf("%s", buf);
+		}
+		return;
+	}
 
-    printf("%s", ptr);
+	printf("%s", ptr);
 }
 
 int install_daemon() {
+	CFErrorRef error           = NULL;
+	AuthorizationItem authItem = {kSMRightBlessPrivilegedHelper, 0, NULL, 0};
+	AuthorizationRights authRights = {1, &authItem};
+	AuthorizationFlags flags = kAuthorizationFlagDefaults | kAuthorizationFlagInteractionAllowed | kAuthorizationFlagPreAuthorize | kAuthorizationFlagExtendRights;
 
-    CFErrorRef error = NULL;
-    AuthorizationItem authItem = { kSMRightBlessPrivilegedHelper, 0, NULL, 0 };
-    AuthorizationRights authRights = { 1, &authItem };
-    AuthorizationFlags flags = kAuthorizationFlagDefaults |
-                               kAuthorizationFlagInteractionAllowed |
-                               kAuthorizationFlagPreAuthorize |
-                               kAuthorizationFlagExtendRights;
+	const char* prompt = "RadicalVPN requires a background service to establish VPN connections.\nIt is not possible to use RadicalVPN without the background service.\n\n";
 
-    const char *prompt = "RadicalVPN requires a background service to establish VPN connections.\nIt is not possible to use RadicalVPN without the background service.\n\n";
+	AuthorizationItem envItems = {kAuthorizationEnvironmentPrompt, strlen(prompt), (void*)prompt, 0};
+	AuthorizationEnvironment env = {1, &envItems};
 
-    AuthorizationItem envItems = {kAuthorizationEnvironmentPrompt, strlen(prompt), (void *)prompt, 0};
-    AuthorizationEnvironment env = { 1, &envItems };
+	AuthorizationRef authRef = NULL;
+	OSStatus err = AuthorizationCreate(&authRights, &env, flags, &authRef);
 
-    AuthorizationRef  authRef = NULL;
-    OSStatus err = AuthorizationCreate(&authRights, &env, flags, &authRef);
+	if (err == errAuthorizationSuccess) {
+		//add the daemon starter the LaunchD
+		bool success = SMJobBless(kSMDomainSystemLaunchd, CFStringCreateWithCString(kCFAllocatorDefault, RADICAL_PACKAGE_NAME, kCFStringEncodingMacRoman), (AuthorizationRef)authRef, &error);
 
-    if(err == errAuthorizationSuccess)
-    {
-        //add the daemon starter the LaunchD
-        bool success = SMJobBless(kSMDomainSystemLaunchd,
-                      CFStringCreateWithCString(kCFAllocatorDefault, RADICAL_PACKAGE_NAME, kCFStringEncodingMacRoman),
-                      (AuthorizationRef) authRef,
-                      &error);
+		AuthorizationFree(authRef, kAuthorizationFlagDefaults);
 
-        AuthorizationFree(authRef, kAuthorizationFlagDefaults);
+		if (success) {
+			printf("Daemon installed.");
+			return 0;
+		} else {
+			log_error_ref(error);
 
-        if (success)
-        {
-            printf("Daemon installed.");
-            return 0;
-        }
-        else
-        {
-            log_error_ref(error);
-            printf("%s", &error);
+			if (error != NULL) {
+				CFRelease(error);
+			}
+			return 1;
+		}
+	}
 
-            if (error != NULL) {
-                CFRelease(error);
-            }
-            return 1;
-        }
-    }
-
-    return 1;
+	return 1;
 }
 
-int get_installed_bundle_path(char* ret_buf, int buf_size) {
-    CFStringRef helper_str = CFStringCreateWithCString(kCFAllocatorDefault, RADICAL_PACKAGE_NAME, kCFStringEncodingMacRoman);
-    CFDictionaryRef retDict = SMJobCopyDictionary(kSMDomainSystemLaunchd, helper_str);
-    if (helper_str != NULL)
-    {
-        CFRelease(helper_str);
-    }
+CFDictionaryRef get_bundle_dictionary(const char* bundle_path) {
+	CFStringRef bundle_str = CFStringCreateWithCString(kCFAllocatorDefault, bundle_path, kCFStringEncodingMacRoman);
+	CFStringRef bundle_str_escaped = CFURLCreateStringByAddingPercentEscapes(NULL, bundle_str, NULL, NULL, kCFStringEncodingUTF8);
+	CFURLRef url        = CFURLCreateWithString(NULL, bundle_str_escaped, NULL);
+	CFDictionaryRef dic = CFBundleCopyInfoDictionaryForURL(url);
 
-    if (retDict == NULL)
-    {
-        return 1;
-    }
+	if (url != NULL) {
+		CFRelease(url);
+	}
 
-    int ret = 0;
-    CFStringRef key = CFStringCreateWithCString(kCFAllocatorDefault, "ProgramArguments", kCFStringEncodingMacRoman);
-    if (CFDictionaryContainsKey(retDict, key))
-    {
-        CFArrayRef program_arguments = CFDictionaryGetValue(retDict, key);
-        if (program_arguments != NULL)
-        {
-            CFStringRef helperBundlePath = CFArrayGetValueAtIndex(program_arguments, 0);
-            if (!CFStringGetCString(helperBundlePath, ret_buf, buf_size, kCFStringEncodingMacRoman))
-            {
-                ret = 2;
-            }
-        }
-        else
-        {
-            ret = 3;
-        }
-    }
-    else
-    {
-        ret = 4;
-    }
+	if (bundle_str != NULL) {
+		CFRelease(bundle_str);
+	}
 
-    if (key != NULL)
-    {
-        CFRelease(key);
-    }
+	return dic;
+}
 
-    if (retDict != NULL)
-    {
-        CFRelease(retDict);
-    }
+int get_bundle_version(const char* bundle_path, char* ret_buf, int buf_size) {
+	CFDictionaryRef dict = get_bundle_dictionary(bundle_path);
+	if (dict == NULL) {
+		return 1;
+	}
 
-    return ret;
+	int ret = 0;
+	CFStringRef key = CFStringCreateWithCString(kCFAllocatorDefault, "CFBundleVersion", kCFStringEncodingMacRoman);
+
+	if (dict != NULL && CFDictionaryContainsKey(dict, key)) {
+		CFStringRef ver = CFDictionaryGetValue(dict, key);
+
+		if (ver == NULL) {
+			ret = 2;
+		} else {
+			if (!CFStringGetCString(ver, ret_buf, buf_size, kCFStringEncodingMacRoman)) {
+				ret = 3;
+			}
+
+			CFRelease(ver);
+		}
+	} else
+		ret = 4;
+
+	if (key != NULL) {
+		CFRelease(key);
+	}
+
+	if (dict != NULL) {
+		CFRelease(dict);
+	}
+
+	return ret;
 }
 
 bool daemon_install_required() {
-    char installed_bundle[256] = {0};
+	char current_version[64]   = {0};
+	char installed_version[64] = {0};
 
-    if (get_installed_bundle_path(installed_bundle, 256) == 0)
-    {
-        printf("%s\n", installed_bundle);
-        return false;
-    }
-    else {
-        return true;
-    }
+	int ret1 = get_bundle_version(BUNDLE_HELPER_BINARY, current_version, sizeof(current_version));
+	int ret2 = get_bundle_version(INSTALLED_HELPER_BINARY, installed_version, sizeof(installed_version));
+
+	if (ret1 == 0 && ret2 == 0) {
+		if (strcmp(current_version, installed_version) == 0) {
+			return false;
+		} else {
+			return true;
+		}
+	} else {
+		return false;
+	}
 }
 
 
-int main(int argc, char **argv) {
-    if (argc >= 2)
-    {
-        if (strcmp(argv[1], "--install") == 0)
-        {
-            printf("Installing daemon..");
-            return install_daemon();
-        }
+int main(int argc, char** argv) {
+	if (argc >= 2) {
+		if (strcmp(argv[1], "--install") == 0) {
+			printf("Installing daemon..");
+			return install_daemon();
+		}
 
-        if (strcmp(argv[1], "--install-required") == 0)
-        {
-            bool install_required = daemon_install_required();
-            install_required ? printf("Daemon install required") : printf("Daemon install not required");
-            return install_required ? 0 : 1;
-        }
-    }
+		if (strcmp(argv[1], "--install-required") == 0) {
+			bool install_required = daemon_install_required();
+			install_required ? printf("Daemon install required") : printf("Daemon install not required");
+			return install_required ? 0 : 1;
+		}
+	}
 
-    // no matching option
-    printf("Invalid usage.\n");
-    printf("Arguments:\n");
-    printf("    --install\n");
-    printf("    --install-required\n");
+	// no matching option
+	printf("Invalid usage.\n");
+	printf("Arguments:\n");
+	printf("    --install\n");
+	printf("    --install-required\n");
 
-    return 1;
+	return 1;
 }
